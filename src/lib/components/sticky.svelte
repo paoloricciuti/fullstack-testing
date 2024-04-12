5de26e9921d5d44ef82f2afdcc8e77f4c7914816
paoloricciuti <ricciutipaolo@gmail.com>
2024-04-12 15:43:51 +0200

    feat: add stiky note and title

---
+++ src/lib/components/sticky.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import roughjs from 'roughjs';
+
+	export let as: keyof HTMLElementTagNameMap = 'div';
+
+	function rough(node: SVGSVGElement) {
+		const svg_el = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+		const svg = roughjs.svg(svg_el);
+		let back: SVGElement;
+		const resize_observer = new ResizeObserver(([entry]) => {
+			console.log(entry);
+			const [{ inlineSize: width, blockSize: height }] = entry.borderBoxSize;
+			svg_el.setAttribute('viewBox', `0 0 ${width} ${height}`);
+			const new_back = svg.rectangle(0, 0, width, height, {
+				fill: 'white',
+				fillStyle: 'solid',
+				strokeWidth: 10,
+				preserveVertices: true
+			});
+			try {
+				svg_el.replaceChild(back, new_back);
+			} catch {
+				svg_el.append(new_back);
+			}
+			back = new_back;
+		});
+		svg_el.style.position = 'absolute';
+		svg_el.style.inset = '0';
+		svg_el.style.zIndex = '-1';
+		svg_el.style.transform = `rotate(${Math.random() * 2 * (Math.random() > 0.5 ? 1 : -1)}deg)`;
+		node.prepend(svg_el);
+		resize_observer.observe(node);
+		return {
+			destroy() {
+				resize_observer.disconnect();
+			}
+		};
+	}
+</script>
+
+<svelte:element this={as} {...$$restProps} class:relative={true} use:rough>
+	<slot />
+</svelte:element>
--- src/slides/100/index.svelte
+++ src/slides/100/index.svelte
@@ -1,8 +1,25 @@
 <script lang="ts" context="module">
+	import Step from '@lib/components/step.svelte';
 	import src from './image.gif';
 	import type { Props } from '@slides/types';
+	import Sticky from '@lib/components/sticky.svelte';
 
 	export const props: Props = {
 		image: src
 	};
 </script>
+
+<Step class="custom rotate">
+	<Sticky as="h1" class="text-8xl uppercase text-slate-900 p-10">Fullstack testing!</Sticky>
+</Step>
+
+<style>
+	:global(.fragment.custom.rotate) {
+		transform: rotate(0) scale(0);
+		transition-duration: 2s;
+	}
+	:global(.fragment.visible.custom.rotate) {
+		transform: rotate(2turn) scale(1);
+		transition-timing-function: cubic-bezier(0.175, 0.885, 0.32, 1.175);
+	}
+</style>
