8d9a13c72c85ebab90d411147c200ec051640fac
paoloricciuti <ricciutipaolo@gmail.com>
2024-04-17 22:55:30 +0200

    feat: other slides

---
+++ src/slides/2200/index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import basic from './basic.png';
+</script>
+
+<div class="bg-black py-8 pl-8">
+	<img src={basic} alt="the basic thing to do" />
+</div>
---
+++ src/slides/2300/index.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Highlight from '@lib/components/highlight.svelte';
+	import Sticky from '@lib/components/sticky.svelte';
+</script>
+
+<div class="grid gap-8">
+	<Sticky as="h1" class="text-slate-950 p-4">Unit</Sticky>
+	<Highlight
+		lang="typescript"
+		code={`
+import { my_simple_function } from './my_simple_function';
+import { it, expect } from 'vitest';
+`}
+	/>
+</div>
