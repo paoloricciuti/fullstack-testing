e85bbbbcd6af4f19c2a3bf8a7d98071bb72aa3a1
paoloricciuti <ricciutipaolo@gmail.com>
2024-04-19 23:47:39 +0200

    chore: remove code component and shiki magic move

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,25 +1,12 @@
-import Code from './code.svelte'
-import FitText from './fit.svelte'
-import Markdown from './markdown.svelte'
-import Media from './media.svelte'
-import Notes from './notes.svelte'
-import Presentation from './presentation.svelte'
-import Slide from './slide.svelte'
-import Stack from './stack.svelte'
-import Step from './step.svelte'
-import Stretch from './stretch.svelte'
-import Vertical from './vertical.svelte'
+import FitText from './fit.svelte';
+import Markdown from './markdown.svelte';
+import Media from './media.svelte';
+import Notes from './notes.svelte';
+import Presentation from './presentation.svelte';
+import Slide from './slide.svelte';
+import Stack from './stack.svelte';
+import Step from './step.svelte';
+import Stretch from './stretch.svelte';
+import Vertical from './vertical.svelte';
 
-export {
-	Code,
-	FitText,
-	Markdown,
-	Media,
-	Notes,
-	Presentation,
-	Slide,
-	Stack,
-	Step,
-	Stretch,
-	Vertical,
-}
+export { FitText, Markdown, Media, Notes, Presentation, Slide, Stack, Step, Stretch, Vertical };
